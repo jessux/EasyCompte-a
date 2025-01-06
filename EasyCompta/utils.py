dd9c04b62bb237004cecc5b9d022299d24bc8d9d
KAHLOUCHE Gabriel <gabriel.kahlouche@groupama.com>
2025-01-06 13:50:43 +0100

    uploads

--- EasyCompta/utils.py
+++ EasyCompta/utils.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def get_pcg():
     # Exemple étendu de données du PCG incluant les classes 6 et 7
-    pcgxls= pd.read_excel(".\\uploads\\pcg.xlsx")
+    pcgxls= pd.read_excel("/workspace/uploads/pcg.xlsx")
     pcgxls["CompteNum"]=pcgxls["CompteNum"].astype(str)
     pcgxls["CompteNum"]=pcgxls["CompteNum"].apply(add_zero_if_shorter)
     return pcgxls
